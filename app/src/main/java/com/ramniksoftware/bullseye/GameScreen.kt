c9afb21c320e3d57ae8d9997feab362ca22bca0a
Niklas Rammerstorfer <rammerstorfer.n@gmail.com>
2026-01-31 13:24:51 +0100

    Optimized imports.

--- app/src/main/java/com/ramniksoftware/bullseye/GameScreen.kt
+++ app/src/main/java/com/ramniksoftware/bullseye/GameScreen.kt
@@ -1,14 +1,11 @@
 package com.ramniksoftware.bullseye
 
-import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -19,7 +16,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
