2ccf027c8c4b539e8a938a0e89082006e183eb0f
Niklas Rammerstorfer <rammerstorfer.n@gmail.com>
2026-01-31 10:59:12 +0100

    Added basic UI.

---
+++ app/src/main/java/com/ramniksoftware/bullseye/GameScreen.kt
@@ -0,0 +1,45 @@
+package com.ramniksoftware.bullseye
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Button
+import androidx.compose.material3.Slider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.ramniksoftware.bullseye.ui.theme.BullseyeTheme
+
+@Composable
+fun GameScreen() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("PUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+        Text("89", fontSize = 32.sp, fontWeight = FontWeight.Bold)
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text("1")
+            Slider(
+                value = 0.5f,
+                valueRange = 0.01f..1f,
+                onValueChange = {}
+            )
+            Text("100")
+        }
+        Button(onClick = {}) {
+            Text("HIT ME")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GameScreenPreview() {
+    BullseyeTheme {
+        GameScreen()
+    }
+}
--- app/src/main/java/com/ramniksoftware/bullseye/MainActivity.kt
+++ app/src/main/java/com/ramniksoftware/bullseye/MainActivity.kt
@@ -5,12 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.ramniksoftware.bullseye.ui.theme.BullseyeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +15,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BullseyeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Niklas",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) { _ ->
+                    GameScreen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BullseyeTheme {
-        Greeting("Android")
-    }
-}
