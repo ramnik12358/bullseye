49145e3ab62becccad46cf198c9f0e8e77ff1a57
Niklas Rammerstorfer <rammerstorfer.n@gmail.com>
2026-02-01 17:40:29 +0100

    Styled texts.

--- app/src/main/java/com/ramniksoftware/bullseye/components/GameDetail.kt
+++ app/src/main/java/com/ramniksoftware/bullseye/components/GameDetail.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -12,6 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.ramniksoftware.bullseye.R
 
 @Composable
@@ -44,7 +46,10 @@ fun GameInfo(label: String, modifier: Modifier = Modifier, value: Int = 0) {
         modifier = modifier.padding(horizontal = 32.dp)
     ) {
         Text(label)
-        Text(value.toString())
+        Text(
+            value.toString(),
+            style = MaterialTheme.typography.labelLarge.copy(fontSize = 20.sp)
+        )
     }
 }
 
--- app/src/main/java/com/ramniksoftware/bullseye/components/GamePrompt.kt
+++ app/src/main/java/com/ramniksoftware/bullseye/components/GamePrompt.kt
@@ -3,12 +3,14 @@ package com.ramniksoftware.bullseye.components
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.ramniksoftware.bullseye.R
@@ -20,12 +22,20 @@ fun GamePrompt(targetValue: Int, modifier: Modifier = Modifier) {
         verticalArrangement = Arrangement.Center,
         modifier = modifier
         ) {
-        Text(stringResource(R.string.instruction_text))
+        Text(
+            stringResource(R.string.instruction_text),
+            style = MaterialTheme.typography.titleMedium.copy(letterSpacing = 1.sp, fontWeight = FontWeight.Bold)
+        )
         Text(
             text = targetValue.toString(),
-            fontSize = 32.sp,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(8.dp)
+            modifier = Modifier.padding(8.dp),
+            style = MaterialTheme.typography.displayMedium.copy(fontWeight = FontWeight.Bold)
         )
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GamePromptPreview() {
+    GamePrompt(targetValue = 50)
 }
